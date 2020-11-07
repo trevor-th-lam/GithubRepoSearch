c1079252c9056eb5ffea0eefd44b17c6c91f59bb
trevor-th-lam <trevor.th.lam@gmail.com>
2020-11-07 17:49:58 +0000

    Fixes: Language is nullable

--- app/src/main/java/com/trevorthlam/codingtest/models/RepoInfo.kt
+++ app/src/main/java/com/trevorthlam/codingtest/models/RepoInfo.kt
@@ -8,8 +8,8 @@ data class RepoInfo(
         val owner: RepoOwner,
         val url: String,
         val created_at: String,
-        val updated_at: String,
-        val language: String,
+        val pushed_at: String,
+        val language: String?,
         val forks_count: Int,
         val open_issues_count: Int
 )
